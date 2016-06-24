9c240874b2dfa5fc544c9a3080b8ec452419201a
Hemanth Yamijala <hyamijala@hortonworks.com>
2016-06-24 18:25:35 +0530

    Modified entity creation code to use referenceable ID for non-composite attributes

--- src/main/java/org/apache/atlas/demo/AtlasEntitiesDemo.java
+++ src/main/java/org/apache/atlas/demo/AtlasEntitiesDemo.java
@@ -4,6 +4,7 @@ import org.apache.atlas.AtlasClient;
 import org.apache.atlas.AtlasServiceException;
 import org.apache.atlas.typesystem.Referenceable;
 import org.apache.atlas.typesystem.json.InstanceSerialization;
+import org.apache.atlas.typesystem.persistence.Id;
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 
@@ -80,7 +81,9 @@ public class AtlasEntitiesDemo implements AtlasDemoConstants {
         anchorCf.set(CF_ATTRIBUTE_IN_MEMORY, true);
         anchorCf.set(CF_ATTRIBUTE_COMPRESSION, "zip");
         anchorCf.set(CF_ATTRIBUTE_BLOCK_SIZE, 128);
-        anchorCf.set(CF_ATTRIBUTE_COLUMNS, Arrays.asList(cssNsiColumn, myLookCaColumn));
+
+        anchorCf.set(CF_ATTRIBUTE_COLUMNS,
+                Arrays.asList(getReferenceableId(cssNsiColumn), getReferenceableId(myLookCaColumn)));
 
         Referenceable contentsCf = new Referenceable(HBASE_COLUMN_FAMILY_TYPE);
         contentsCf.set(AtlasClient.REFERENCEABLE_ATTRIBUTE_NAME, "default.webtable.contents@cluster1");
@@ -91,14 +94,14 @@ public class AtlasEntitiesDemo implements AtlasDemoConstants {
         contentsCf.set(CF_ATTRIBUTE_IN_MEMORY, false);
         contentsCf.set(CF_ATTRIBUTE_COMPRESSION, "lzo");
         contentsCf.set(CF_ATTRIBUTE_BLOCK_SIZE, 1024);
-        contentsCf.set(CF_ATTRIBUTE_COLUMNS, Arrays.asList(htmlColumn));
+        contentsCf.set(CF_ATTRIBUTE_COLUMNS, Arrays.asList(getReferenceableId(htmlColumn)));
 
         Referenceable webTable = new Referenceable(HBASE_TABLE_TYPE);
         webTable.set(AtlasClient.REFERENCEABLE_ATTRIBUTE_NAME, WEBTABLE_NAME);
         webTable.set(AtlasClient.NAME, "webtable");
         webTable.set(AtlasClient.DESCRIPTION, "Table that stores crawled information");
         webTable.set(TABLE_ATTRIBUTE_IS_ENABLED, true);
-        webTable.set(TABLE_ATTRIBUTE_NAMESPACE, new Referenceable(namespaceId, HBASE_NAMESPACE_TYPE, null));
+        webTable.set(TABLE_ATTRIBUTE_NAMESPACE, getReferenceableId(namespaceId, HBASE_NAMESPACE_TYPE));
         webTable.set(TABLE_ATTRIBUTE_COLUMN_FAMILIES, Arrays.asList(anchorCf, contentsCf));
 
         List<Referenceable> entities =
@@ -106,11 +109,14 @@ public class AtlasEntitiesDemo implements AtlasDemoConstants {
 
         JSONArray entitiesJson = new JSONArray(entities.size());
 
+        System.out.print("[");
         for (Referenceable entity : entities) {
             String entityJson = InstanceSerialization.toJson(entity, true);
-            System.out.println(entityJson);
+            System.out.print(entityJson);
+            System.out.println(",");
             entitiesJson.put(entityJson);
         }
+        System.out.println("]");
 
         List<String> entitiesCreated = atlasClient.createEntity(entities);
         for (String entity : entitiesCreated) {
@@ -119,6 +125,15 @@ public class AtlasEntitiesDemo implements AtlasDemoConstants {
         Utils.printDelimiter();
         return entitiesCreated.get(entitiesCreated.size()-1);
     }
+
+    private Id getReferenceableId(Referenceable fullReferenceable) {
+        return getReferenceableId(fullReferenceable.getId()._getId(), fullReferenceable.getTypeName());
+    }
+
+    private Id getReferenceableId(String id, String typeName) {
+        return new Id(id, 0, typeName);
+    }
+
 
     private String createNamespace() throws AtlasServiceException {
         System.out.println("Creating namespace entity");
